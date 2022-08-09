a8785d9d2d1312a0473995443b5b3d7f081467f2
iasmincorrea <iasmin_correa@hotmail.com>
2022-08-09 14:54:36 -0300

    Página de serviços

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.use("/assets", express.static("assets"))
+app.use("/assets", express.static("assets"));
 
 app.listen(3500, () => {
   console.log("Servidor rodando!");
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/pages/index.html");
+});
+
+app.get("/servicos", (req, res) => {
+  res.sendFile(__dirname + "/pages/services.html");
 });
