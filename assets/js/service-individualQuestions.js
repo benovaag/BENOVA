7ed1009b6c25336a21c054b33479990872cb8c6d
iasmincorrea <iasmin_correa@hotmail.com>
2022-08-12 17:59:47 -0300

    Página One-Stop Shop

---
+++ assets/js/service-individualQuestions.js
@@ -0,0 +1,24 @@
+function initServices(elem, option) {
+  document.addEventListener("click", function (e) {
+    if (!e.target.matches(elem + " .service-questions__title")) return;
+    else {
+      if (
+        !e.target.parentElement.classList.contains("service-questions__active")
+      ) {
+        if (option == true) {
+          var elementList = document.querySelectorAll(
+            elem + " .services-items"
+          );
+          Array.prototype.forEach.call(elementList, function (e) {
+            e.classList.remove("service-questions__active");
+          });
+        }
+        e.target.parentElement.classList.add("service-questions__active");
+      } else {
+        e.target.parentElement.classList.remove("service-questions__active");
+      }
+    }
+  });
+}
+
+initServices(".service-questions__item", true);
